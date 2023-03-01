438d7546798a2beab446ada92ee5bcca25669371
fanzhen <f13152349319@163.com>
2023-03-01 22:33:48 +0800

    新加2

--- src/main/java/com/po/test.java
+++ src/main/java/com/po/test.java
@@ -4,5 +4,6 @@ public class test  {
     public static void main(String[] args) {
         System.out.println("第一次测试");
         System.out.println("asd");
+        System.out.println("测试二");
     }
 }
